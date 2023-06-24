d98110749ad2334d45dec6ee0c599100f58d2387
dennadwic <ddc.16.12.95@gmail.com>
2023-06-24 23:18:08 +0700

    add social media

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .skills-column, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-img, .skills-row, .portfolio-box, .contact form', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
