876c0be18393117e3d5706f16143c473a1000109
Denna Dwi C <44631660+dennadwic@users.noreply.github.com>
2023-06-24 23:07:57 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,13 +48,13 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-img, .skills-column, .portfolio-box, .contact form', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['IT Support', 'IT Infrastructure', 'DevOps Engineer'],
+    strings: ['IT Support', 'IT Infrastructure', 'DevOps Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
